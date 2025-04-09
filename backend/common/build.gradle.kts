d99f12a6361edae86d769b7d3f55f001251cad78
Christian Bergschneider <christian.bergschneider@gmx.de>
2025-04-09 10:01:09 +0200

    feat: build and publish

--- backend/common/build.gradle.kts
+++ backend/common/build.gradle.kts
@@ -1,4 +1,65 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
 dependencies {
     compileOnly(libs.annotations)
     compileOnly(libs.slf4j.simple)
+}
+
+// Publish to Everbuild Maven
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            repositories {
+                maven {
+                    credentials {
+                        username = property("username")!! as String
+                        password = property("password")!! as String
+                    }
+                    setUrl("https://mvn.everbuild.org/public")
+                }
+            }
+            from(components["java"])
+            pom {
+                url.set("https://github.com/4drian3d/SignedVelocity")
+                licenses {
+                    license {
+                        name.set("GNU General Public License version 3 or later")
+                        url.set("https://opensource.org/licenses/GPL-3.0")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/4drian3d/SignedVelocity.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/4drian3d/SignedVelocity.git")
+                    url.set("https://github.com/4drian3d/SignedVelocity")
+                }
+                developers {
+                    developer {
+                        id.set("4drian3d")
+                        name.set("Adrian Gonzales")
+                        email.set("adriangonzalesval@gmail.com")
+                    }
+                }
+                issueManagement {
+                    name.set("GitHub")
+                    url.set("https://github.com/4drian3d/SignedVelocity/issues")
+                }
+                ciManagement {
+                    name.set("GitHub Actions")
+                    url.set("https://github.com/4drian3d/SignedVelocity/actions")
+                }
+                name.set(project.name)
+                description.set(project.description)
+                url.set("https://github.com/4drian3d/SignedVelocity")
+            }
+            artifactId = "signedvelocity-minestom"
+        }
+    }
 }
--- backend/minestom/build.gradle.kts
+++ backend/minestom/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     `java-library`
     `maven-publish`
-    signing
 }
 
 java {
@@ -19,64 +18,56 @@ dependencies {
     testImplementation(libs.slf4j.simple)
 }
 
-// Publish to Maven Central
-//publishing {
-//    publications {
-//        create<MavenPublication>("maven") {
-//            repositories {
-//                maven {
-//                    credentials {
-//                        username = property("sonatypeTokenUsername")?.toString() ?: ""
-//                        password = property("sonatypeTokenPassword")?.toString() ?: ""
-//                    }
-//                    val central = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-//                    val snapshots = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-//                    if (project.version.toString().endsWith("SNAPSHOT")) {
-//                        name = "SonatypeSnapshots"
-//                        setUrl(snapshots)
-//                    } else {
-//                        name = "OSSRH"
-//                        setUrl(central)
-//                    }
-//                }
-//            }
-//            from(components["java"])
-//            pom {
-//                url.set("https://github.com/4drian3d/SignedVelocity")
-//                licenses {
-//                    license {
-//                        name.set("GNU General Public License version 3 or later")
-//                        url.set("https://opensource.org/licenses/GPL-3.0")
-//                    }
-//                }
-//                scm {
-//                    connection.set("scm:git:https://github.com/4drian3d/SignedVelocity.git")
-//                    developerConnection.set("scm:git:ssh://git@github.com/4drian3d/SignedVelocity.git")
-//                    url.set("https://github.com/4drian3d/SignedVelocity")
-//                }
-//                developers {
-//                    developer {
-//                        id.set("4drian3d")
-//                        name.set("Adrian Gonzales")
-//                        email.set("adriangonzalesval@gmail.com")
-//                    }
-//                }
-//                issueManagement {
-//                    name.set("GitHub")
-//                    url.set("https://github.com/4drian3d/SignedVelocity/issues")
-//                }
-//                ciManagement {
-//                    name.set("GitHub Actions")
-//                    url.set("https://github.com/4drian3d/SignedVelocity/actions")
-//                }
-//                name.set(project.name)
-//                description.set(project.description)
-//                url.set("https://github.com/4drian3d/SignedVelocity")
-//            }
-//            artifactId = "signedvelocity-minestom"
-//        }
-//    }
-//}
+// Publish to Everbuild Maven
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            repositories {
+                maven {
+                    credentials {
+                        username = property("username")?.toString() ?: ""
+                        password = property("password")?.toString() ?: ""
+                    }
+                    setUrl("https://mvn.everbuild.org/public")
+                }
+            }
+            from(components["java"])
+            pom {
+                url.set("https://github.com/4drian3d/SignedVelocity")
+                licenses {
+                    license {
+                        name.set("GNU General Public License version 3 or later")
+                        url.set("https://opensource.org/licenses/GPL-3.0")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/4drian3d/SignedVelocity.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/4drian3d/SignedVelocity.git")
+                    url.set("https://github.com/4drian3d/SignedVelocity")
+                }
+                developers {
+                    developer {
+                        id.set("4drian3d")
+                        name.set("Adrian Gonzales")
+                        email.set("adriangonzalesval@gmail.com")
+                    }
+                }
+                issueManagement {
+                    name.set("GitHub")
+                    url.set("https://github.com/4drian3d/SignedVelocity/issues")
+                }
+                ciManagement {
+                    name.set("GitHub Actions")
+                    url.set("https://github.com/4drian3d/SignedVelocity/actions")
+                }
+                name.set(project.name)
+                description.set(project.description)
+                url.set("https://github.com/4drian3d/SignedVelocity")
+            }
+            artifactId = "signedvelocity-minestom"
+        }
+    }
+}
 //signing {
 //    useGpgCmd()
 //    sign(configurations.archives.get())
